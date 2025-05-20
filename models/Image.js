a49191cc42abddec8cbb01a10ae42e14ad2bd406
tluc2502 <tluc2502@gmail.com>
2025-05-21 00:14:40 +0700

    đổi model img

--- models/Image.js
+++ models/Image.js
@@ -2,8 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
-  variantID: { type: Schema.Types.ObjectId, ref: 'ProductVariant', required: true },
-  imageURL: { type: String, required: true }
+  productID: { 
+    type: Schema.Types.ObjectId, 
+    ref: 'Product', 
+    required: true 
+  },
+  imageURL: { 
+    type: [String], // Mảng các đường dẫn ảnh
+    required: true,
+    validate: {
+      validator: function(urls) {
+        // Đảm bảo mảng có ít nhất 1 phần tử
+        return urls && urls.length > 0;
+      },
+      message: 'Phải có ít nhất một đường dẫn ảnh'
+    }
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Image', ImageSchema);
