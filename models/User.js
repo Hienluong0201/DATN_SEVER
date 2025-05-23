586ff874c2a7d5b06d9f88beac05215fe60f747f
Hienluong0201 <nguyenhienluong200212@gmail.com>
2025-05-23 23:18:43 +0700

    thêm đăng nhập bằng otp

--- models/User.js
+++ models/User.js
@@ -5,12 +5,14 @@ const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: String },
+  phone: { type: String, required: true, unique: true  },
   img: { type: String },
   role: { type: String, default: 'user' },
   isActive: { type: Boolean, default: true },
   resetPasswordCode: String,      
-  resetPasswordExpires: Date       
+  resetPasswordExpires: Date,  
+  otpCode: String, 
+  otpExpires: Date 
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
